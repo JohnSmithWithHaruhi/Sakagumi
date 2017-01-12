647198afd9e392d9c8ae854eb610f87b9c00fbea
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2017-01-12 22:05:41 +0900

    把BottomNavigation的內容全部都刪除

--- app/src/main/java/johnsmithwithharuhi/co/nogikeya/MainActivity/MainActivityView.java
+++ app/src/main/java/johnsmithwithharuhi/co/nogikeya/MainActivity/MainActivityView.java
@@ -4,66 +4,35 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import johnsmithwithharuhi.co.nogikeya.Blog.FragmentView;
-import johnsmithwithharuhi.co.nogikeya.News.NewsFragmentView;
+import johnsmithwithharuhi.co.nogikeya.Blog.BlogMainFragment;
 import johnsmithwithharuhi.co.nogikeya.R;
 import johnsmithwithharuhi.co.nogikeya.databinding.ActivityMainBinding;
 
 public class MainActivityView extends AppCompatActivity {
 
-  private Fragment mCurrentFragment = null;
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     ActivityMainBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-    showFragment("Blog", new FragmentView());
+    getSupportFragmentManager().beginTransaction()
+        .add(R.id.main_content, new BlogMainFragment())
+        .commit();
     BottomNavigationView bottomNavigationView = mBinding.mainBottomNavigation;
     bottomNavigationView.setOnNavigationItemSelectedListener(
         new BottomNavigationView.OnNavigationItemSelectedListener() {
           @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
               case R.id.action_blog:
-                showFragment(item.getTitle().toString(), new FragmentView());
-                break;
               case R.id.action_rss:
-                showFragment(item.getTitle().toString(), FragmentView.newInstance(11));
-                break;
               case R.id.action_event:
               case R.id.action_member:
               case R.id.action_favorite:
-                showFragment(item.getTitle().toString(), new NewsFragmentView());
                 break;
             }
             return true;
           }
         });
-  }
-
-  private void showFragment(String tag, Fragment newFragment) {
-    if (mCurrentFragment != null) {
-      getSupportFragmentManager().beginTransaction()
-          .detach(mCurrentFragment)
-          .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-          .commit();
-    }
-    Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
-    if (fragment != null) {
-      getSupportFragmentManager().beginTransaction()
-          .attach(fragment)
-          .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-          .commit();
-      mCurrentFragment = fragment;
-    } else {
-      getSupportFragmentManager().beginTransaction()
-          .add(R.id.main_content, newFragment, tag)
-          .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-          .commit();
-      mCurrentFragment = newFragment;
-    }
   }
 }
